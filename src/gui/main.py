65f70e3ab37430c2a42caad71e9cf9256f0d0429
Håvard Gulldahl <havard@lurtgjort.no>
2011-04-01 02:10:54 +0200

    now adding up per total use per song

--- src/gui/main.py
+++ src/gui/main.py
@@ -5,25 +5,10 @@ import sys
 from PyQt4.uic import loadUi
 import PyQt4.QtGui as Gui
 import PyQt4.QtCore as Core
+import PyQt4.Qt as Qt
 
 import xmeml
 
-
-def audiblesecs(clipitem, values, threshold=0.1):
-  secs = 0.0
-  prev = 0.0
-  if isinstance(values, basestring): # single value for whole clipitem
-    if float(values) > threshold:
-      secs = float(clipitem.duration) / clipitem.timebase
-  else: # a list of keyframes and respective volume level
-    # add the (implicit) keyframe end point
-    keyframelist = values[:]
-    keyframelist += (clipitem.duration, values[-1][1]),
-    for keyframe, volume in keyframelist:
-      if float(volume) < threshold: continue
-      secs += (float(keyframe) - prev) / clipitem.timebase
-      prev = float(keyframe)
-  return secs
 
 class Odometer(Gui.QMainWindow):
   UIFILE="pling-plong-odometer.ui"
@@ -33,31 +18,33 @@ class Odometer(Gui.QMainWindow):
 
   def __init__(self, xmemlfile,parent=None):
     super(Odometer, self).__init__(parent)
-    self.xmeml = xmeml.VideoSequence(file=xmemlfile)
-    #self.app = Gui.QApplication(argv)
+    self.xmemlfile = xmemlfile
     self.ui = loadUi(self.UIFILE, self)
     self.ui.loadFileButton.clicked.connect(self.clicked)
     self.rowCreated.connect(self.lookuprow)
 
   def clicked(self, qml):
-    print "clicked"
+    audioclips = {}
+    self.xmeml = xmeml.VideoSequence(file=self.xmemlfile)
     for c in self.xmeml.track_items:
       if c.id is None or c.type != 'clipitem' or c.mediatype != 'audio': continue
-      if not c.name.startswith("SCD"): continue
-      for f in c.filters:
-        if f.id != "audiolevels": continue
-        for param in f.parameters:
-          if param.values:
-            secs = "%.1fs" % audiblesecs(c, param.values)
-          else:
-            secs = "%.1fs" % audiblesecs(c, param.value)
-
-          r = Gui.QTreeWidgetItem(self.ui.clips, ['', c.name, secs, '...'])
-          self.rows.append(r)
-          self.rowCreated.emit(r)
+      if not c.name.startswith("SCD"): continue # HACK alert
+      secs = c.audiblesecs()
+      if not audioclips.has_key(c.file.id): 
+        audioclips[c.file.id] = secs
+      else:
+        audioclips[c.file.id] += secs
+        
+    for audioclip, length in audioclips.iteritems():
+      r = Gui.QTreeWidgetItem(self.ui.clips, ['', audioclip, "%.1fs" % length, '...'])
+      r.setData(0, Core.Qt.ItemIsUserCheckable, True)
+      r.setCheckState(0, Core.Qt.Checked)
+      self.rows.append(r)
+      self.rowCreated.emit(r)
 
   def lookuprow(self, r):
-    print "lookup row: ", r
+    #print "lookup row: ", r
+    pass
 
   def run(self, app):
     self.ui.show()
