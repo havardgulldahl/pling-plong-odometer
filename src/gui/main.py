f7b43f4f3c31c15a8ccf7620dad28a2b07bfbf8b
Håvard Gulldahl <havard@lurtgjort.no>
2011-04-03 22:35:21 +0200

    Showing all subclips in UI, also computing frames, not only secs

--- src/gui/main.py
+++ src/gui/main.py
@@ -11,47 +11,55 @@ import xmeml
 
 
 class Odometer(Gui.QMainWindow):
-  UIFILE="pling-plong-odometer.ui"
+    UIFILE="pling-plong-odometer.ui"
 
-  rows = []
-  rowCreated = Core.pyqtSignal(['QTreeWidgetItem'], name="rowCreated")
+    rows = []
+    rowCreated = Core.pyqtSignal(['QTreeWidgetItem'], name="rowCreated")
 
-  def __init__(self, xmemlfile,parent=None):
-    super(Odometer, self).__init__(parent)
-    self.xmemlfile = xmemlfile
-    self.ui = loadUi(self.UIFILE, self)
-    self.ui.loadFileButton.clicked.connect(self.clicked)
-    self.rowCreated.connect(self.lookuprow)
+    def __init__(self, xmemlfile,parent=None):
+        super(Odometer, self).__init__(parent)
+        self.xmemlfile = xmemlfile
+        self.ui = loadUi(self.UIFILE, self)
+        self.ui.loadFileButton.clicked.connect(self.clicked)
+        self.rowCreated.connect(self.lookuprow)
 
-  def clicked(self, qml):
-    audioclips = {}
-    self.xmeml = xmeml.VideoSequence(file=self.xmemlfile)
-    for c in self.xmeml.track_items:
-      if c.id is None or c.type != 'clipitem' or c.mediatype != 'audio': continue
-      if not c.name.startswith("SCD"): continue # HACK alert
-      secs = c.audiblesecs()
-      if not audioclips.has_key(c.file.id): 
-        audioclips[c.file.id] = secs
-      else:
-        audioclips[c.file.id] += secs
-        
-    for audioclip, length in audioclips.iteritems():
-      r = Gui.QTreeWidgetItem(self.ui.clips, ['', audioclip, "%.1fs" % length, '...'])
-      r.setData(0, Core.Qt.ItemIsUserCheckable, True)
-      r.setCheckState(0, Core.Qt.Checked)
-      self.rows.append(r)
-      self.rowCreated.emit(r)
+    def keyPressEvent(self, event):
+        if event.key() == Core.Qt.Key_Escape:
+            self.close()
 
-  def lookuprow(self, r):
-    #print "lookup row: ", r
-    pass
+    def clicked(self, qml):
+        audioclips = {}
+        self.xmeml = xmeml.VideoSequence(file=self.xmemlfile)
+        for c in self.xmeml.track_items:
+            if c.id is None or c.type != 'clipitem' or c.mediatype != 'audio': continue
+            #if not c.file.mediatype == 'audio': continue
+            if not c.id.startswith("SCD"): continue
+            if not audioclips.has_key(c.file.id): 
+                audioclips[c.file.id] = [c,]
+            else:
+                audioclips[c.file.id] += [c,]
+                
+        for audioclip, pieces in audioclips.iteritems():
+            length = 12.2
+            r = Gui.QTreeWidgetItem(self.ui.clips, ['', audioclip, "%.1fs" % length, '...'])
+            r.setData(0, Core.Qt.ItemIsUserCheckable, True)
+            r.setCheckState(0, Core.Qt.Checked)
+            self.rows.append(r)
+            self.rowCreated.emit(r)
+            for subclip in pieces:
+                sr = Gui.QTreeWidgetItem(r, ['', subclip.id, "%s - %s" % (subclip.start(), subclip.end())])
+                r.addChild(sr)
 
-  def run(self, app):
-    self.ui.show()
-    sys.exit(app.exec_())
+    def lookuprow(self, r):
+        #print "lookup row: ", r
+        pass
+
+    def run(self, app):
+        self.ui.show()
+        sys.exit(app.exec_())
 
 if __name__ == '__main__':
-  app = Gui.QApplication(sys.argv)
-  o = Odometer("../../data/fcp.sample.xml")
-  o.run(app)
+    app = Gui.QApplication(sys.argv)
+    o = Odometer("../../data/fcp.sample.xml")
+    o.run(app)
 
