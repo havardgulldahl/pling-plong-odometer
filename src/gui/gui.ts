615843cc133bbe31672f8197c4d2d2cddd9acb22
Håvard Gulldahl <havard@gullbar.local>
2011-05-04 15:37:21 +0200

    adding translator files

---
+++ src/gui/gui.ts
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nb_NO" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message utf8="true">
+        <location filename="pling-plong-odometer.ui" line="18"/>
+        <source>♫ ♪ Odometer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="27"/>
+        <source>Project</source>
+        <translation>Prosjekt</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="33"/>
+        <source>Production no.</source>
+        <translation>Prodnummer</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="49"/>
+        <source>Drop a Final Cut Pro sequence (XML export) anywhere to load it.</source>
+        <translation>Slipp en Final Cut Pro-sekvens (XML-eksport) på et vilkårlig sted for å laste den inn.</translation>
+    </message>
+    <message utf8="true">
+        <location filename="pling-plong-odometer.ui" line="77"/>
+        <source>♫</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="82"/>
+        <source>Clip name</source>
+        <translation>Klippnavn</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="87"/>
+        <source>Duration</source>
+        <translation>Varighet</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="92"/>
+        <source>Metadata</source>
+        <translation>Metadata</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="100"/>
+        <source>Errors and warnings</source>
+        <translation>Feil og advarsler</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="113"/>
+        <source>Details</source>
+        <translation>Detaljer</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="122"/>
+        <source>&amp;Title</source>
+        <translation>&amp;Tittel</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="135"/>
+        <source>A&amp;rtist</source>
+        <translation>A&amp;rtist</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="148"/>
+        <source>C&amp;omposer</source>
+        <translation>&amp;Forfatter</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="161"/>
+        <source>&amp;Year</source>
+        <translation>&amp;År</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="187"/>
+        <source>Track &amp;number</source>
+        <translation>&amp;Spor</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="200"/>
+        <source>Copyri&amp;ght</source>
+        <translation>Copyri&amp;ght</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="213"/>
+        <source>&amp;Label</source>
+        <translation>&amp;Plateselskap</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="226"/>
+        <source>Close the Details box</source>
+        <translation>Lukk Detaljer</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="243"/>
+        <source>Locate a Final Cut Pro sequence (XML export) and load it.</source>
+        <translation>Finn en Final Cut Pro-sekvens (XML-eksport) og last den inn.</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="246"/>
+        <source>&amp;Open file...</source>
+        <translation>Å&amp;pne fil...</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="253"/>
+        <source>Select a threshold in dB</source>
+        <translation>Velg et grensenivå i dB</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="256"/>
+        <source>All audio below this value is left out when the total duration of each clip is computed.</source>
+        <translation>All lyd under denne verdien utelates når den totale klipplengden regnes ut.</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="259"/>
+        <source> dB</source>
+        <translation>dB</translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="262"/>
+        <source>&gt; </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="319"/>
+        <source>Til &amp;PRF</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="329"/>
+        <source>Til &amp;AUX</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="pling-plong-odometer.ui" line="339"/>
+        <source>&amp;Rulletekst</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
