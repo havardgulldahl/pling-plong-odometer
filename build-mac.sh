5a5c100f42349fa4dac927ccf3a11f3b9c96bd87
Håvard Gulldahl <Havard.Gulldahl@nrk.no>
2013-03-03 23:35:56 +0100

    adapting to new box and new os

--- build-mac.sh
+++ build-mac.sh
@@ -15,6 +15,7 @@ function error {
 DROPBOXPATH=/Volumes/Media/dropbox/Dropbox;
 DROPBOXURL=http://dl.dropbox.com/u/12128173/Odometer;
 VERSION=$(date +"%Y-%m-%d");
+PYQTPATH=/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/PyQt4/uic
 
 # change bulid defaults
 sed -i .bk "s/beta=.*/beta=0/" BUILDFLAGS
@@ -24,13 +25,14 @@ sed -i .bk "s/releaseCheck=.*/releaseCheck=0/" BUILDFLAGS
 
 echo "Generating translations for UX"
 #pylupdate4-2.7r 
-pylupdate4-2.7 src/gui/gui.pro || error "pylupdate failed";
+pylupdate src/gui/gui.pro || error "pylupdate failed";
 lrelease src/gui/gui.pro || error "lrelease failed";
 
 echo "Generating code for UX"
-pyuic4-2.7 -o src/gui/odometer_ui.py src/gui/pling-plong-odometer.ui || error "pyuic failed"
-pyuic4-2.7 -o src/gui/auxreport_ui.py src/gui/pling-plong-auxreport.ui || error "pyuic failed"
-pyuic4-2.7 -o src/gui/prfreport_ui.py src/gui/pling-plong-prfreport.ui || error "pyuic failed"
+python $PYQTPATH/pyuic.py -o src/gui/odometer_ui.py src/gui/pling-plong-odometer.ui || error "pyuic failed"
+python $PYQTPATH/pyuic.py -o src/gui/auxreport_ui.py src/gui/pling-plong-auxreport.ui || error "pyuic failed"
+python $PYQTPATH/pyuic.py -o src/gui/prfreport_ui.py src/gui/pling-plong-prfreport.ui || error "pyuic failed"
+exit
 
 # store settings in files, to be picked up by pyqt resource system
 echo "$DROPBOXURL" > ./DROPBOXURL;
