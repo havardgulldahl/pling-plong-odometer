85fca7d1e8b86c236d6e6852131e9ac58954e3f0
Håvard Gulldahl <havard@gulldahl.no>
2016-09-02 00:03:27 +0200

    Build packages for OS X

--- build-mac.sh
+++ build-mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file is part of odometer by Håvard Gulldahl <havard.gulldahl@nrk.no>
-# (C) 2011-2012
+# (C) 2011-2016
 
 function error {
     echo "Plonk! Something went wrong:";
@@ -12,10 +12,7 @@ function error {
 
 # some settings
 
-DROPBOXPATH=/Volumes/Media/Dropbox;
-DROPBOXURL=http://dl.dropbox.com/u/12128173/Odometer;
 VERSION=$(date +"%Y-%m-%d");
-#NIB=/Library/Frameworks/QtGui.framework/Versions/4/Resources/qt_menu.nib
 NIB=./resources/qt_menu.nib
 
 echo "Generating version: $VERSION";
@@ -26,37 +23,15 @@ sed -i .bk "s/releaseCheck=.*/releaseCheck=0/" BUILDFLAGS
 
 # update all generated code
 
-echo "Generating translations for UX"
-#pylupdate4-2.7r
-pylupdate4 src/gui/gui.pro || error "pylupdate failed";
-lrelease src/gui/gui.pro || error "lrelease failed";
+python ./buildpyqt.py
 
-echo "Generating code for UX"
-pyuic4 -o src/gui/odometer_ui.py src/gui/pling-plong-odometer.ui || error "pyuic failed"
-pyuic4 -o src/gui/auxreport_ui.py src/gui/pling-plong-auxreport.ui || error "pyuic failed"
-pyuic4 -o src/gui/prfreport_ui.py src/gui/pling-plong-prfreport.ui || error "pyuic failed"
-pyuic4 -o src/gui/onlinelogin_ui.py src/gui/pling-plong-onlinelogin.ui || error "pyuic failed"
-
-# store settings in files, to be picked up by pyqt resource system
-echo "$DROPBOXURL" > ./DROPBOXURL;
-echo "$VERSION" > ./VERSIONMAC;
-pyrcc4 -o src/gui/odometer_rc.py src/gui/odometer.qrc || error "pyrcc failed"
-
-# clean up old cruft
-echo "Removing old code"
-rm -rf ./build ./dist ./pling-plong-odometer.dmg || error "cleanup failed"
-
-# update xmeml
-(cd ../xmeml && git pull);
-cp -fr ../xmeml/xmeml src/ || error "couldnt find xmeml library";
 
 # build the castle
 echo "Building the app (see build.log)"
-python2.7 setup.py py2app > build.log || error "py2app failed"
+python setup.py py2app > build.log || error "py2app failed"
 
 # changing back defaults
 sed -i .bk "s/beta=.*/beta=1/" BUILDFLAGS
-pyrcc4 -o src/gui/odometer_rc.py src/gui/odometer.qrc || error "pyrcc failed"
 
 # add some missing pieces
 echo "Adding some extra resources"
@@ -76,12 +51,6 @@ hdiutil create "$DMGNAME" -volname "♫ ♪ Odometer" -fs "HFS+" -srcfolder "dis
 echo "Creating .pkg installer";
 ./macromanconv.py ABOUT build/ABOUT.txt
 /usr/local/bin/packagesbuild -v Odometer.pkgproj || error "Packagemaker failed";
-
-# create history
-git tag -a "v$VERSION-mac" -m "Version $VERSION release" || error "couldnt tag git tree";
-git push --tags || error "problems pushing tags to central repository";
-git commit ./VERSIONMAC -m "build-mac.sh: commiting new mac version $VERSION" || error "problems pushing changes to central repository";
-
 
 echo "Finished. Take a look at $DMGNAME"
 echo "Installer in dist/";
