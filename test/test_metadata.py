bbe4967c021479df075e5bdeb16e3f67b036be64
Håvard Gulldahl <havard@gulldahl.no>
2016-09-02 21:44:02 +0200

    Test filenames from all music providers

--- test/test_metadata.py
+++ test/test_metadata.py
@@ -1,6 +1,6 @@
 
 import metadata
-from metadata.resolvers import DMAResolver, AUXResolver
+from metadata.resolvers import DMAResolver, AUXResolver, ApollomusicResolver, UprightmusicResolver, UniPPMResolver
 
 def test_resolveDMA():
     _filenames = {"NONRO716733SG0201": "NONRO716733SG0201 Ofelastema.wav",
@@ -22,3 +22,35 @@ def test_resolveAUX():
     for musicid, filename in _filenames.iteritems():
         assert AUXResolver.musicid(filename) == musicid
 
+def test_resolveApollo():
+    _filenames = {"MUM_124_17": "Apollo_MUM_124_17__Trail_Rider__Donnelly,_Steve.mp3",
+                  "DWCD_514_3": "Apollo_DWCD_514_3__Big_Game_Hunter__Sir_Bald_Diddley.mp3",
+                  "NVS_102_27": "Apollo_NVS_102_27__Unyielding__Dan_Elias_Brevig.mp3",
+                  "SL_60_37": "Apollo_SL_60_37__Hey_Mack_(rhythm_section)__Matthew_David_Waldrum.mp3"}
+
+    for musicid, filename in _filenames.iteritems():
+        assert ApollomusicResolver.musicid(filename) == musicid
+
+def test_resolveUpright():
+    _filenames = {"EDS_016_006": "_UPRIGHT_EDS_016_006_Downplay_(Main).WAV",
+                  "EDS_016_011": "_UPRIGHT_EDS_016_011_Jagged_Edge_(Main).WAV",
+                  "EDS_016_011": "_UPRIGHT_EDS_016_011_Jagged_Edge_(Main).MP3"}
+
+    for musicid, filename in _filenames.iteritems():
+        assert UprightmusicResolver.musicid(filename) == musicid
+
+def test_resolveUniPPM():
+    _filenames = {"777051": "KOK_2360_32_Lightning_Drone_Chevalier_777051",
+                  "627163": "KOS_97_11_Rap_And_Scratch_Andreasen_Kalfayan_627163.wav",
+                  "714159": "RNM_50_9_You_Ives_714159.wav",
+                  "863434": "VTMA_26_33_Lonely_Traveller_863434.wav",
+                  "25175": "EDGE_21_63_Slow_Downer_Cunningham_Fox_Lang_25175.wav",
+                  "896746": "STSC_77_14_A_Million_Copeland_Darnell_Desmond_Kalayeh_896746.wav",
+                  "762667": "STFTA_1_175_Don_t_Take_Me_Down_Hart_Williams_762667.wav",
+                  "858425": "BR_586_5_Unstoppable_Force_Britton_858425.wav",
+                  "652177": "CHAP_386_2_Neapolitan_Love_Song_Stott_652177.wav",
+                  "897593": "RDR_18_5_Sunshine_Shapes_Instrumental_Keane_897593.wav",
+                  "898428": "BER_1250_89_Stress_mit_Recht_Muller_898428.wav"}
+
+    for musicid, filename in _filenames.iteritems():
+        assert UniPPMResolver.musicid(filename) == musicid
