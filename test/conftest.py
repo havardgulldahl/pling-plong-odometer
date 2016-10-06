3fe9d5bab9dd9fced6582e6b8550c34d79c703d6
Håvard Gulldahl <havard@gulldahl.no>
2016-10-06 23:25:25 +0200

    fix unippm resolver signature

--- test/conftest.py
+++ test/conftest.py
@@ -39,6 +39,8 @@ def xmemlfiles(request):
               "920961": "SUN_3_2_Neon_Pulse_Keaton_Thomas_920961.wav",
               "321775": "LO_CD_18_6_East_Of_West_Black_Carpenter_More_321775.wav",
               "762667": "STFTA_1_175_Don_t_Take_Me_Down_Hart_Williams_762667.wav",
+              "912768": "SOHO_153_6_Liquor_To_Pandas_Ghettososcks_Lamb_Ross_912768.wav",
+
 
               }
 
