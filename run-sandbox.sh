915678c0444b4415b14b523d5b40edbd347975b0
Håvard Gulldahl <havard@gulldahl.no>
2017-02-20 21:10:46 +0100

    Remove deprecated files

--- run-sandbox.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH=/usr/bin:/bin:/usr/sbin:/sbin;
-
-PROG="${1:-dist/♫ ♪ Odometer.app/Contents/MacOS/Pling Plong Odometer}";
-
-sandbox-exec -f odometer-sandbox.sb "${PROG}";
-
