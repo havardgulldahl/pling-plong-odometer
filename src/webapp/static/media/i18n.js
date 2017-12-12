37f8a9b7563392432fa4ca9feb1900b7d48a61be
Håvard Gulldahl <havard.gulldahl@nrk.no>
2017-12-12 10:50:58 +0100

    adding i18n to html ui

---
+++ src/webapp/static/media/i18n.js
@@ -0,0 +1,85 @@
+var messages = {
+    /*
+    'en-US': {
+        NUM_PHOTOS: 'You have {numPhotos, plural, ' +
+            '=0 {no photos.}' +
+            '=1 {one photo.}' +
+            'other {# photos.}}'
+    },
+
+    'no_NO': {
+        NUM_PHOTOS: 'Usted {numPhotos, plural, ' +
+            '=0 {no tiene fotos.}' +
+            '=1 {tiene una foto.}' +
+            'other {tiene # fotos.}}'
+    }
+    */
+    'en-US': {
+        CLOSE: 'Close',
+        FILE_OK: 'File is OK ({FILESIZE})',
+        REPORT_HEADER_PRF: "Track metadata sheet for PRF",
+        REPORT_HEADER_TIMELINE: "Tracks by order of entry on timeline",
+        SECONDS_IN_TOTAL: "Seconds in total: {SECONDS}",
+        IN: "In",
+        OUT: "Out",
+        DURATION: "Duration",
+        CLIP_DETAILS: "Clip details",
+        GETTING_METADATA: "Getting metadata...",
+        UNKNOWN: "Unknown",
+        END_CREDITS: "End credits", 
+        FULL_FILE_NAME: "Full file name: {FILENAME}",
+        NOT_VALID_FILE_TYPE: "Not a valid file type.",
+        DOWNLOAD_AS_FILE: "Download as file",
+        METADATA: "Metadata",
+        CLOSE: "Close",
+        NO_FILES_SELECTED: "No files selected",
+        ALERTMSG: "Server replied {ERRCODE}: {ERRMSG}",
+        ANALYSIS: "Analysis",
+        HELP: "Help",
+        DOCS: "API docs",
+        CHOOSE_TIMELINE_DATA: "Choose timeline data (XMEML)",
+        PLEASE_SELECT_FILE: "Please select a file", 
+        GENERATE_METADATA_REPORT: "Generate metadata report",
+        GENERATE_END_CREDITS: "Generate end credits",
+        NAME: "name",
+        FILENAME: "File name",
+        AUDIBLE_LENGTH: "audible length",
+        MEASURED_IN_SECONDS: "Measured in seconds",
+        MUSIC_LIBRARY: "Music library",
+        METADATA: "metadata"
+    },
+    'no-NO': {
+        CLOSE: 'Lukk',
+        FILE_OK: 'Fila er OK ({FILESIZE})',
+        REPORT_HEADER_PRF: "Lydklipp metadata til PRF",
+        REPORT_HEADER_TIMELINE: "Lydklipp ordnet etter slik de kommer på tidslinja",
+        SECONDS_IN_TOTAL: "Sekunder totalt: {SECONDS}",
+        IN: "Inn",
+        OUT: "Ut",
+        DURATION: "Varighet",
+        CLIP_DETAILS: "Detaljer om klippet",
+        GETTING_METADATA: "Henter metadata...",
+        UNKNOWN: "Ukjent",
+        END_CREDITS: "Rulletekst",
+        FULL_FILE_NAME: "Fult filnavn: {FILENAME}",
+        NOT_VALID_FILE_TYPE: "Ugyldig filtype.",
+        DOWNLOAD_AS_FILE: "Last ned som fil",
+        METADATA: "Metadata",
+        CLOSE: "Lukk",
+        NO_FILES_SELECTED: "Ingen filer er valgt",
+        ALERTMSG: "Serveren svarte med {ERRCODE}: {ERRMSG}",
+        ANALYSIS: "Analyse",
+        HELP: "Hjelp",
+        DOCS: "API doku",
+        CHOOSE_TIMELINE_DATA: "Velg tidslinje-data (XMEML)",
+        PLEASE_SELECT_FILE: "Vennligst velg en fil",
+        GENERATE_METADATA_REPORT: "Lag metadata-rapport",
+        GENERATE_END_CREDITS: "Lag rulletekst",
+        NAME: "navn",
+        FILENAME: "Filnavn",
+        AUDIBLE_LENGTH: "hørbar varighet",
+        MEASURED_IN_SECONDS: "Målt i sekunder",
+        MUSIC_LIBRARY: "Musikkbibliotek",
+        METADATA: "metadata"
+    }
+};
