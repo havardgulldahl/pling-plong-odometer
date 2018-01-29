4fd7318db0caa90e25bf0e8d2dd26558bcd8968b
Håvard Gulldahl <havard.gulldahl@nrk.no>
2018-01-29 23:00:47 +0100

    better output

--- deploy_latest.sh
+++ deploy_latest.sh
@@ -2,30 +2,64 @@
 
 # deploy latest odometer
 
+RESTORE=$(echo -en '\033[0m')
+RED=$(echo -en '\033[00;31m')
+GREEN=$(echo -en '\033[00;32m')
+YELLOW=$(echo -en '\033[00;33m')
+BLUE=$(echo -en '\033[00;34m')
+MAGENTA=$(echo -en '\033[00;35m')
+PURPLE=$(echo -en '\033[00;35m')
+CYAN=$(echo -en '\033[00;36m')
+LIGHTGRAY=$(echo -en '\033[00;37m')
+LRED=$(echo -en '\033[01;31m')
+LGREEN=$(echo -en '\033[01;32m')
+LYELLOW=$(echo -en '\033[01;33m')
+LBLUE=$(echo -en '\033[01;34m')
+LMAGENTA=$(echo -en '\033[01;35m')
+LPURPLE=$(echo -en '\033[01;35m')
+LCYAN=$(echo -en '\033[01;36m')
+WHITE=$(echo -en '\033[01;37m')
+
+
+function result {
+    echo ${GREEN}$*${RESTORE};
+}
+
+function info {
+    echo ${MAGENTA}$*${RESTORE};
+}
+
+function err {
+    echo ${RED}$*${RESTORE};
+    exit 1;
+}
+
 cd /tmp;
 
-#1
-# get latest release
-echo "Get latest release from Github.";
+info "Get latest release from Github.";
 
 LATEST=$(curl -s https://api.github.com/repos/havardgulldahl/pling-plong-odometer/releases/latest \
     | grep tarball_url \
     | cut -d '"' -f 4);
 
 VERSION=$(basename "$LATEST");
-echo "got $LATEST -> $VERSION";
-curl -L -o "${VERSION}.tar" "$LATEST" && echo "Downloaded $VERSION";
+result "got $LATEST -> $VERSION";
+curl -s -L -o "${VERSION}.tar" "$LATEST" || err "Could not download latest tar: $LATEST";
+result "Downloaded $VERSION";
 
 OUTPUT=/tmp;
-echo "Unwrap into ${OUTPUT}/$VERSION";
+info "Unwrap into ${OUTPUT}/$VERSION";
 
 mkdir -p "$OUTPUT/$VERSION";
-tar -xvf "$VERSION.tar" --strip 1 -C "${OUTPUT}/${VERSION}" && echo "unpacked";
+tar -xf "$VERSION.tar" --strip 1 -C "${OUTPUT}/${VERSION}" || err "Untarring $VERSION.tar failed";
+result "unpacked";
 
-echo "Setting up zymlink"
+info "Setting up zymlink"
 
-ln -sf ${OUTPUT}/$VERSION ${OUTPUT}/latest;
+ln -snf ${OUTPUT}/$VERSION ${OUTPUT}/latest || err "Symlinking failed";
+result "symlinked";
 
-echo "restarting odometer server with new version";
+info "restarting odometer server with new version";
 
-systemctl restart odometer@{1..2} && echo "Running version $LATEST";
+systemctl restart odometer@{1..2} || err "Systemd restart failed";
+result "Running version $VERSION";
