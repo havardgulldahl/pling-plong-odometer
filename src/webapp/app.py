45dc4cffff722a45b266dc1e7f36159fa46ccbef
Håvard Gulldahl <havard.gulldahl@nrk.no>
2018-05-29 14:34:21 +0200

    fix api urls

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -203,7 +203,7 @@ async def handle_analyze_post(request):
         ]
     })
 
-@routes.get('/api/supported_resolvers')
+@routes.get('/api/supported_resolvers/')
 @swagger_path("handle_supported_resolvers.yaml")
 async def handle_supported_resolvers(request):
     'GET a request and return a dict of currently suppported resolvers and their file patterns'
@@ -211,7 +211,7 @@ async def handle_supported_resolvers(request):
         'resolvers': getAllResolvers()
     })
 
-@routes.post('/api/feedback')
+@routes.post('/api/feedback/')
 async def handle_feedback_post(request):
     'POST form data feedback. No returned content.'
     data = await request.post() 
@@ -333,7 +333,7 @@ async def handle_get_tracklist(request):
     return web.json_response({'error':[],
                               'tracks': tracklist})
 
-@routes.get('/api/license_rules')
+@routes.get('/api/license_rules/')
 async def handle_get_license_rules(request):
     'Return a json list of all license rules from the DB'
     schema = model.LicenseRule()
@@ -367,7 +367,7 @@ async def handle_get_licenses(request):
     'Show all license rules in a view'
     return {}
 
-@routes.get('/api/feedback')
+@routes.get('/api/feedback/')
 async def handle_get_feedback_api(request):
     'Return a json list of all license rules from the DB'
     schema = model.Feedback()
@@ -398,7 +398,7 @@ def copyright_owner(request):
     return {}
 
 
-@routes.get('/api/missing_filenames')
+@routes.get('/api/missing_filenames/')
 async def handle_get_missing_filenames_api(request):
     'Return a json list of all reported missing filenames from the DB'
     schema = model.ReportedMissing()
