92cdf503482ecb1851321f3811ef911e4d8fe126
Håvard Gulldahl <havard.gulldahl@nrk.no>
2018-06-21 01:02:10 +0200

    add script to parse ifpi members

--- scripts/parse_ifpi_members.py
+++ scripts/parse_ifpi_members.py
@@ -6,24 +6,25 @@ import sys
 from urllib.request import urlopen
 import lxml.html
 
-URL='https://www.fono.no/medlemmene/'
+URL='http://www.ifpi.no/ifpi-norge/ifpi-medlemmer'
 
 def iterMembers():
     html = lxml.html.parse(urlopen(URL))
-    for row in html.getroot().xpath("//div[@class='members']//a"): #cssselect(".members li a"):
+    for row in html.getroot().xpath("//div[starts-with(@class, 'item')]/div/p/strong/strong"): 
         txt = row.text_content().strip()
         yield txt
         if txt.endswith(' AS'): # also add item without AS suffix
             yield txt[:-3]
 
+
 if __name__ == '__main__':
     print("BEGIN;")
-    print("DELETE FROM license_rule WHERE source='FONO';")
+    print("DELETE FROM license_rule WHERE source='IFPI';")
     for r in iterMembers():
         print("""INSERT INTO license_rule 
                 (source, license_property, license_status, license_value, comment)
                 VALUES 
-                ('FONO', 'label', 'green', '{}', '{}');""".format(r, URL))
+                ('IFPI', 'label', 'green', '{}', '{}');""".format(r, URL))
     print("COMMIT;")
 
 
